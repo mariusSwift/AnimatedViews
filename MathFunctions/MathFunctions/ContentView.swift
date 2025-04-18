bb148b3a174c297ce145b39d212dd7659de33667
Marius <143952464+mariusSwift@users.noreply.github.com>
2025-04-18 22:17:34 +0200

    Add files via upload

--- MathFunctions/MathFunctions/ContentView.swift
+++ MathFunctions/MathFunctions/ContentView.swift
@@ -2,14 +2,14 @@
 //  ContentView.swift
 //  MathFunctions
 //
-//  Created by Marius on 05/04/2025.
+//  Created by Marius on 18/04/2025.
 //
 
 import SwiftUI
 
 struct Signal: Shape {
     
-    // animation of the signal
+    // facilitates animation
     var animatableData: Double {
         get { offset }
         set { self.offset = newValue }
@@ -20,42 +20,44 @@ struct Signal: Shape {
     var frequency: Double
     var offset: Double
 
-    // conforming to Shape protocol
+    // creating path (Shape protocol conformity only prerequisite)
     func path(in rect: CGRect) -> Path {
         let curve = UIBezierPath()
 
         // some specified points
-        let width = Double(rect.width)
-        let height = Double(rect.height)
-        let midWidth = width / 2
-        let midHeight = height / 2
-        let oneOverMidWidth = 1 / midWidth
+        let screenWidth = Double(rect.width)
+        let screenHeigth = Double(rect.height)
+        let screenMiddleWidth = screenWidth / 2
+        let screenMiddleHeigth = screenHeigth / 2
 
-        // calculate the length between 2 peaks
-        let wavelength = width / frequency
+        // length between 2 peaks (wavelength)
+        let lambda = screenWidth / frequency
 
         // start from one side and middle screen
-        curve.move(to: CGPoint(x: 0, y: midHeight))
+        curve.move(to: CGPoint(x: 0, y: screenMiddleHeigth))
 
         // iterate across each horizontal pixel
-        for x in stride(from: 0, through: width, by: 1) {
-            // current position in respect to the wavelength
-            let relativeX = x / wavelength
+        for x in stride(from: 0, through: screenWidth, by: 1) {
+            
+            // x position in respect to wavelength
+            let positionX = x / lambda
 
-            // distance from x axis centre
-            let distanceFromMidWidth = x - midWidth
+            // distance of x from middle screen
+            let distanceX = x - screenMiddleWidth
 
-            // normalize it and create reversed parabola
-            let normalDistance = oneOverMidWidth * distanceFromMidWidth
-            let parabola = -(normalDistance * normalDistance) + 1
+            // normalize calculated distance (value betweeen -1 and 1)
+            let distanceNormX = distanceX / screenMiddleWidth
+            
+            // determine the inverted parabolic curve
+            let parabola = -(distanceNormX * distanceNormX) + 1
 
-            // find sine and add phase
-            let sine = sin(relativeX + offset)
+            // find sine of x position and phase taken into account
+            let sine = sin(positionX + offset)
 
-            // determine the y_axis point
-            let y = parabola * amplitude * sine + midHeight
+            // determine the value on y_axis
+            let y = sine * amplitude * parabola + screenMiddleHeigth
 
-            // add a line up to this point
+            // add a line up to current (x,y) point
             curve.addLine(to: CGPoint(x: x, y: y))
         }
         return Path(curve.cgPath)
@@ -69,7 +71,7 @@ struct ContentView: View {
         ZStack {
             let colors: [Color] = [.yellow, .green]
             ForEach(0..<2) { i in
-                Signal(amplitude: 50, frequency: 25, offset: self.offset + Double(i) * .pi)
+                Signal(amplitude: 80, frequency: 20, offset: self.offset + Double(i) * .pi)
                     .stroke(colors[i], lineWidth: 5)
             }
         }
